f58988606ea5e9f0d0a667d600827803e7e82921
diegourrego <diego.urrego@mercadolibre.com.co>
2023-12-26 21:30:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,4 +5,9 @@ import "fmt"
 func main()  {
 	fmt.Println("Esto es una prueba")
 	fmt.Println("Esto es otra prueba")
+	greetTest("Diego")
+}
+
+func greetTest(name string) {
+	fmt.Println("Hola", name)
 }
